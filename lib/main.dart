b3e4cdbb4ad824f2024570a5d24ea98190357f64
rhuanunivel <rhuanunivel@gmail.com>
2026-04-06 22:05:36 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:birdle/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
+}
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
 
 class MainApp extends StatelessWidget {
